01c6c82be129168995952f2e1b4bb760a1e8bb59
yuji okayama <y.okayama.13@gmail.com>
2024-08-28 22:39:46 +0900

    インポート更新

--- src/app/ui/page.tsx
+++ src/app/ui/page.tsx
@@ -2,13 +2,16 @@
 
 import Button from "@/components/atoms/Button";
 import Form from "@/components/molecules/Form";
+import AsyncComponent from "@/components/molecules/AsyncComponent";
+
 
 export default function UiTest() {
   return (
     <>
       <h1>UIテスト</h1>
-      {/* <Button label="ボタン" onClick={() => alert("クリック")} /> */}
+      <Button label="ボタン" onClick={() => alert("クリック")} />
       <Form />
+      <AsyncComponent />
     </>
   );
 }
