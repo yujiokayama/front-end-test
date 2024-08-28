5d71d3a61cdf1b40e8d84476eea49e9c0bc80327
yuji okayama <y.okayama.13@gmail.com>
2024-08-28 22:39:37 +0900

    hookのテスト

---
+++ src/app/__test__/hooks/useCounter.test.tsx
@@ -0,0 +1,25 @@
+import { renderHook, act } from "@testing-library/react";
+
+import useCounter from "@/hooks/useCounter";
+
+describe("useCounter", () => {
+  it("インクリメントのテスト", () => {
+    const { result } = renderHook(() => useCounter(0));
+
+    act(() => {
+      result.current.increment();
+    });
+
+    expect(result.current.count).toBe(1);
+  });
+
+  it("デクリメントのテスト", () => {
+    const { result } = renderHook(() => useCounter(0));
+
+    act(() => {
+      result.current.decrement();
+    });
+
+    expect(result.current.count).toBe(-1);
+  });
+});
