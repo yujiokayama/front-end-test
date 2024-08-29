b6ed214273b98718b938bd2dc5b491ca125db0a8
yuji okayama <y.okayama.13@gmail.com>
2024-08-29 22:47:38 +0900

    改行

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -14,7 +14,9 @@ export async function GET() {
 export async function POST(request: Request) {
   const body = await request.json();
   const newTodo = { id: todos.length + 1, ...body };
+
   todos.push(newTodo);
+
   return NextResponse.json(newTodo);
 }
 
@@ -22,7 +24,9 @@ export async function POST(request: Request) {
 export async function PUT(request: Request) {
   const body = await request.json();
   const { id } = body;
+
   todos = todos.map((todo) => (todo.id === id ? { ...todo, ...body } : todo));
+
   return NextResponse.json(todos.find((todo) => todo.id === id));
 }
 
@@ -30,6 +34,8 @@ export async function PUT(request: Request) {
 export async function DELETE(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = parseInt(searchParams.get("id") || "0", 10);
+
   todos = todos.filter((todo) => todo.id !== id);
+
   return NextResponse.json({ id });
 }
